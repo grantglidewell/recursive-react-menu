9d29a6f3303f4ee3cf95a800664530478a3d5bd8
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-30 10:37:28 -0700

    changed node to pure component, ~8% faster perf

--- src/App/Nav/Node/Node.js
+++ src/App/Nav/Node/Node.js
@@ -1,43 +1,46 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import styles from "./Node.less";
 
-export function Node({
-  active,
-  selected,
-  closed,
-  collapsed,
-  depth,
-  path,
-  icon,
-  name,
-  children,
-  handleOpen
-}) {
-  // style if a node is active
-  const isActive = (active && styles.active) || "";
-  // style is a node is selected
-  const isSelected = (selected === path && styles.selected) || "";
-  // check if a parent node is collapsed
-  const isClosed = closed && styles.closed;
-  const isCollapsed =
-    collapsed && collapsed.filter(collapsed => collapsed === path).length;
-  return (
-    <li
-      className={`${styles.item} ${isActive} ${
-        styles[`depth${depth}`]
-      } ${isSelected} ${isClosed}`}
-      key={path}
-    >
-      <a href={`/${path}`}>
-        <i className={`fa fa-${icon}`} />
-        <span>{name}</span>
-      </a>
-      {children && (
-        <i
-          className={isCollapsed ? "fa fa-caret-left" : "fa fa-caret-down"}
-          onClick={() => handleOpen(path)}
-        />
-      )}
-    </li>
-  );
+export class Node extends PureComponent {
+  render() {
+    const {
+      active,
+      selected,
+      closed,
+      collapsed,
+      depth,
+      path,
+      icon,
+      name,
+      children,
+      handleOpen
+    } = this.props;
+    // style if a node is active
+    const isActive = (active && styles.active) || "";
+    // style is a node is selected
+    const isSelected = (selected === path && styles.selected) || "";
+    // check if a parent node is collapsed
+    const isClosed = closed && styles.closed;
+    const isCollapsed =
+      collapsed && collapsed.filter(collapsed => collapsed === path).length;
+    return (
+      <li
+        className={`${styles.item} ${isActive} ${
+          styles[`depth${depth}`]
+        } ${isSelected} ${isClosed}`}
+        key={path}
+      >
+        <a href={`/${path}`}>
+          <i className={`fa fa-${icon}`} />
+          <span>{name}</span>
+        </a>
+        {children && (
+          <i
+            className={isCollapsed ? "fa fa-caret-left" : "fa fa-caret-down"}
+            onClick={() => handleOpen(path)}
+          />
+        )}
+      </li>
+    );
+  }
 }
